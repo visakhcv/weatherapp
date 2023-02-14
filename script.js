4275e91ed8d088bca67e344b51ff512ce5386782
visakhcv <visakhvisa12345@gmail.com>
2023-02-14 18:14:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+async function main(){
+    let location=cinput.value
+    if(location){
+    const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=8ac5c4d57ba6a4b3dfcf622700447b1e&units=metric`)
+    response.json().then((data)=> {
+        console.log(data);
+        displaydata(data)
+        tempdata(data)
+    })
+}
+    else{
+        alert('Enter the location')
+    }
+} 
+
+function displaydata(weather){
+    htmldata=`
+    <div class="row">
+    <div class="pricing-column">
+      <div class="card bg-transparent">
+        <div class="card-header bg-dark">
+          <h3>${weather.name}</h3>
+        </div>
+
+        <div class="card-body">
+          <h5>Cloud <span>: ${weather.clouds.all} </span> </h5>
+          <h5>Humidity <span>: ${weather.main.humidity} </span> </h5>
+          <h5>Wind <span style="margin-left:32px">: ${weather.wind.speed} </span></h5>
+          <h5>Pressure <span style="margin-left:5px">: ${weather.main.pressure} </span></h5>
+
+        </div>
+      </div>
+    </div>
+</div>
+    `
+    leftcontent.innerHTML = htmldata
+
+ 
+
+}
+
+function tempdata(temperature){
+    maindata=`
+    <div id="mainicon">
+                <h1><span class="icon"> <img class="iconimg"  src='http://openweathermap.org/img/w/${temperature.weather[0].icon}.png'> </span> </h1>
+            </div>
+            <div id="temp">
+                <h1 style="font-size:80px">${temperature.main.temp} &#8451; </h1>
+                <h5>Feels like ${temperature.main.feels_like}  &#8451;</h5>
+                <h4>${temperature.weather[0].main}</h4>
+                <h2>${temperature.name}</h2>
+                <h5>${temperature.sys.country}</h5>
+            </div>
+    `
+    maincol.innerHTML = maindata
+}
